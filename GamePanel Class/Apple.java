784a166db4939c8ecc7009c55c67b637a398b012
lab596 <86579136+lab596@users.noreply.github.com>
2022-02-14 21:17:00 -0800

    Update and rename newApple.java to Apple.java

--- GamePanel Class/Apple.java
+++ GamePanel Class/Apple.java
@@ -5,3 +5,16 @@ public void newApple(){
         //sets the apple y location to an int that is random and within the screen height and is the size of a game pixel
         appleYLoc = random.nextInt((int)(SCREEN_HEIGHT/PIXEL_SIZE))*PIXEL_SIZE;
     }
+
+//check apple method
+public void checkApple(){
+        //sees if the x and y coordinate of the player is the same as coordinates of apple
+        if((x[0] == appleXLoc) && (y[0] == appleYLoc)){
+            //increases size of the snake
+            size ++;
+            //increases points that the player has
+            points ++;
+            //randomly adds a new apple to the game board
+            newApple();
+        }
+    }
